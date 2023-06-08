3a93b60fd2b9f9cf87fb9e844a87c204115afe26
lexslie <lesliiee727@gmail.com>
2023-06-08 15:46:34 -0700

    Update index.js & template.js
    fixed syntax errors

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const DIST_DIR = path.resolve(__dirname, "dist");
+const distPath = path.join(DIST_DIR, "team.html");
+const generateTeam = require("./src/template")
  
 
 teamArray = [];
@@ -121,7 +122,7 @@ function init () {
     // Function to create the html file
     function htmlBuilder () {
         console.log("Your team has been successfully created!")
-        fs.writeFileSync(outputPath, generateTeam(teamArray), "UTF-8")
+        fs.writeFileSync(distPath, generateTeam(teamArray), "UTF-8")
     }
 
     createTeam();
--- src/template.js
+++ src/template.js
@@ -2,6 +2,7 @@ const generateTeam = team => {
     
     // Generating manager card
     const generateManager = manager => {
+        `
         <div class="card employee-card"> 
             <div class="card-header">
                 <h2 class="card-title">${manager.getName()}</h2>
@@ -9,16 +10,18 @@ const generateTeam = team => {
             </div>
             <div class="card-body">
                 <ul class="list-group">
-                    <li class="list-group-item">ID: ${manager.getID()}</li>
+                    <li class="list-group-item">ID: ${manager.getId()}</li>
                     <li class="list-group-item">Email: <a>${manager.getEmail()}</a></li>
                     <li class="list-group-item">Office Number: ${manager.getofficeNumber()}</li>
                 </ul>
             </div>
         </div>;
+        `;
     };
 
     // Generating engineer card
     const generateEngineer = engineer => {
+        `
         <div class="card employee-card">
             <div class="card-header">
                 <h2 class="card-title">${engineer.getName()}</h2>
@@ -26,16 +29,18 @@ const generateTeam = team => {
             </div>
             <div class="card-body">
                 <ul class="list-group">
-                    <li class="list-group-item">ID: ${engineer.getID()}</li>
+                    <li class="list-group-item">ID: ${engineer.getId()}</li>
                     <li class="list-group-item">Email: <a>${engineer.getEmail()}</a></li>
                     <li class="list-group-item">GitHub Username: ${engineer.getGithub()}</li>
                 </ul>
             </div>
         </div>;
+        `;
     };
 
     // Generating intern card
     const generateIntern = intern => {
+        `
         <div class="card employee-card">
             <div class="card-header">
                 <h2 class="card-title">${intern.getName()}</h2>
@@ -43,12 +48,13 @@ const generateTeam = team => {
             </div>
             <div class="card-body">
                 <ul class="list-group">
-                    <li class="list-group-item">ID: ${intern.getID()}</li>
+                    <li class="list-group-item">ID: ${intern.getId()}</li>
                     <li class="list-group-item">Email: <a>${intern.getEmail()}</a></li>
                     <li class="list-group-item">School: ${intern.getSchool()}</li>
                 </ul>
             </div>
         </div>;
+        `;
     };
 
     // Creating the team using declared variables
