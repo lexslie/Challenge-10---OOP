7f123e348962e2d117ece19444713dc62f683653
lexslie <lesliiee727@gmail.com>
2023-06-08 11:11:15 -0700

    Update index.js
    added function to initialize the app

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+teamArray = [];
+
+// Function to initialize app
+function init () {
+    inquirer.prompt ([{
+        type: "list",
+        message: "What type of employee do you want to add onto your team?",
+        name: "addEmployeePrompt",
+        choices: ["Manager", "Engineer", "Intern", "None"]
+    }]).then(function (userInput) {
+        switch(userInput.addEmployeePrompt) {
+            case "Manager":
+                addManager();
+                break;
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Intern":
+                addIntern();
+                break;
+
+            default:
+                htmlBuilder();
+        }
+    })
+}
