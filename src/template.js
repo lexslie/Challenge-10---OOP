5bb909786bcf5b076004671838c8435cf7d6d168
lexslie <lesliiee727@gmail.com>
2023-06-08 14:21:19 -0700

    Update template.js
    added the function to generate completed html page

--- src/template.js
+++ src/template.js
@@ -71,3 +71,34 @@ const generateTeam = team => {
 
     return html.join("");
 }
+
+// Function to generate completed html page
+module.exports = team => {
+
+    return `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Team Profiles</title>
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="header">
+                    <h1>Team Profiles</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                ${generateTeam(team)}
+            </div>
+        </div>
+    </body>
+</html>`;
+};
